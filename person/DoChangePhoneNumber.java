6c143a53f495c79f6f67d92b91571d8ec9597f61
Beatriz Martins <mariabeatriz.dongamartins@gmail.com>
2018-12-05 01:14:04 +0000

    Add files via upload

--- person/DoChangePhoneNumber.java
+++ person/DoChangePhoneNumber.java
@@ -6,15 +6,12 @@ import sth.SchoolManager;
 import sth.exceptions.NoSuchPersonIdException;
 import sth.app.exceptions.NoSuchPersonException;
 
-//FIXME import other classes if needed
-
 /**
  * 4.2.2. Change phone number.
  */
 public class DoChangePhoneNumber extends Command<SchoolManager> {
 
   Input<String> _newPhoneNumber;
-  //FIXME add input fields if needed
 
   /**
    * @param receiver
@@ -23,13 +20,11 @@ public class DoChangePhoneNumber extends Command<SchoolManager> {
     super(Label.CHANGE_PHONE_NUMBER, receiver);
 
     _newPhoneNumber = _form.addStringInput(Message.requestPhoneNumber());
-    //FIXME initialize input fields if needed
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() {
-    //FIXME implement command
     _form.parse();
     _receiver.changePhoneNumber(_newPhoneNumber.value());
     _display.popup(_receiver.showPerson());
--- person/DoSearchPerson.java
+++ person/DoSearchPerson.java
@@ -4,16 +4,12 @@ import pt.tecnico.po.ui.Command;
 import pt.tecnico.po.ui.Input;
 import sth.SchoolManager;
 
-//FIXME import other classes if needed
-
 /**
  * 4.2.4. Search person.
  */
 public class DoSearchPerson extends Command<SchoolManager> {
 
-  //FIXME add input fields if needed
   Input<String> _name;
-
  
   /**
    * @param receiver
@@ -21,7 +17,6 @@ public class DoSearchPerson extends Command<SchoolManager> {
   public DoSearchPerson(SchoolManager receiver) {
     super(Label.SEARCH_PERSON, receiver);
     _name = _form.addStringInput(Message.requestPersonName());
-    //FIXME initialize input fields if needed
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
@@ -29,13 +24,5 @@ public class DoSearchPerson extends Command<SchoolManager> {
   public final void execute() {
     _form.parse();
     _display.popup(_receiver.searchPerson(_name.value()));
-    /*try{
-    _display.popup(_receiver.searchPerson(_name.value()));
-    /*} catch(NoSuchPersonException e){
-        throw new NoSuchPersonException(_name.value());
-    }*/
   }
-
-    //FIXME implement command
-
 }
